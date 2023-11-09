3f5e3dea57f7f3269b9785e3c3dc2e339f1c2751
Mateusz Prusaczyk <mateusz.prusaczyk@gmail.com>
2023-11-09 14:33:19 +0100

    play around with request handler

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello API")
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		log.Println("Request received")
+		body, err := io.ReadAll(req.Body)
+
+		if err != nil {
+			http.Error(res, "Error ocurred", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(res, "Hello %s", body)
 	})
 	http.ListenAndServe(":9234", nil)
 }
