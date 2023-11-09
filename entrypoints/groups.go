5af958fed11d587338bab7bf820d8c7b3829bbb7
Mateusz Prusaczyk <mateusz.prusaczyk@gmail.com>
2023-11-09 16:31:08 +0100

    implement differentiation of http method

--- entrypoints/groups.go
+++ entrypoints/groups.go
@@ -15,6 +15,15 @@ func NewGroups(logger *log.Logger) *Groups {
 }
 
 func (entrypoint *Groups) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		entrypoint.getProducts(res, req)
+		return
+	}
+
+	res.WriteHeader(http.StatusNotImplemented)
+}
+
+func (entrypoint *Groups) getProducts(res http.ResponseWriter, req *http.Request) {
 	groups := groups.GetGroups()
 
 	err := groups.ToJSON(res)
